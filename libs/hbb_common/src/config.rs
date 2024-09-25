839025331a12c0693b149436a37dfd0b826fc71f
minesyyds <149929227+minesyyds@users.noreply.github.com>
2024-09-25 15:59:30 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["hkpro.qncdn.cn"];
-pub const PUBLIC_RS_PUB_KEY: &str = "NAjw30Ha7WpoTuTUWeUyk9v9mpbWgkFpoJv84MaVAYY=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["baidu.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "NAjw30Ha7WpoTuTUWeUdsafgegeqwrwqrdsfsfgkFpoJv84MaVAYY=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
